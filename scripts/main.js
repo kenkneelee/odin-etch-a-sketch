0d357280bc6a155e231b728fde3a9550bc653a30
Kenny Lee <kenny.lee927@gmail.com>
2021-12-01 15:54:05 -0500

    made responsively resizing grid boxes

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,25 @@
 const container = document.querySelector('#container');
+let sides = 200;
 
-for (let i=0;i<256;i++) {
-const box = document.createElement('div');
-box.classList.add('box');
-box.textContent = i;
-container.appendChild(box);
+for (let i = 0; i < (sides ** 2); i++) {
+    let size=100/sides;
+    console.log(size);
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.style.width = size + '%';
+    box.style.height = size + '%';
+    box.textContent = "";
+    container.appendChild(box);
 }
 
 
 const boxes = document.querySelectorAll('.box');
 
-boxes.forEach((box) => { 
+boxes.forEach((box) => {
     box.addEventListener('mouseover', () => {
-        box.style.background="black";
+        box.style.background = "black";
     })
     box.addEventListener('mouseout', () => {
-        box.style.background="beige";
+        box.style.color = "aquamarine";
     })
 })
