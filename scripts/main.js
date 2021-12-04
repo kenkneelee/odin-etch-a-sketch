2e8a9361e55a73bce1a28dcbfdc6b3025840cd43
Kenny Lee <kenny.lee927@gmail.com>
2021-12-03 20:10:25 -0500

    wrote function to change background to random colour

--- scripts/main.js
+++ scripts/main.js
@@ -6,16 +6,16 @@ document.onload = game(16);
 
 
 function game(sides) {
-for (let i = 0; i < (sides ** 2); i++) {
-    let size=100/sides;
-    console.log(size);
-    const box = document.createElement('div');
-    box.classList.add('box');
-    box.style.width = size + '%';
-    box.style.height = size + '%';
-    box.textContent = "";
-    container.appendChild(box);
-}
+    for (let i = 0; i < (sides ** 2); i++) {
+        let size = 100 / sides;
+        console.log(size);
+        const box = document.createElement('div');
+        box.classList.add('box');
+        box.style.width = size + '%';
+        box.style.height = size + '%';
+        box.textContent = "";
+        container.appendChild(box);
+    }
 }
 
 const boxes = document.querySelectorAll('.box');
@@ -23,16 +23,29 @@ boxes.forEach((box) => {
     box.addEventListener('mouseover', () => {
         box.style.background = "black";
     })
-    box.addEventListener('mouseout', () => {
-        box.style.color = "aquamarine";
+    box.addEventListener('mouseout', function () {
+        //setTimeout(function () { box.style.background = "lightgrey" }, 500);
     })
 })
 
 
-document.getElementById("clear").onclick = function() {clear()};
+
+document.getElementById("clear").onclick = function () { clear() };
 
 function clear() {
     boxes.forEach((box) => {
-        box.style.background="lightgrey";
+        box.style.background = "lightgrey";
     })
 }
+
+
+document.getElementById("changebg").onclick = function () { changebg() };
+
+function changebg() {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    boxes.forEach((box) => {
+    box.style.backgroundColor = '#' + randomColor;
+    }
+    )
+}
+
