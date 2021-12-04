ae38056473065ca4a8f35112c1a53b0d3ff9c7a1
Kenny Lee <kenny.lee927@gmail.com>
2021-12-03 21:00:18 -0500

    functions and buttons updated for delete grid, change bg colour, reset grid, and create new grid

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,16 @@
 const container = document.querySelector('#container');
 let sides = 16;
 
-document.onload = game(16);
+document.onload = load();
 
-
+function load() {
+    game(16);
+    etch();
+}
 
 function game(sides) {
     for (let i = 0; i < (sides ** 2); i++) {
         let size = 100 / sides;
-        console.log(size);
         const box = document.createElement('div');
         box.classList.add('box');
         box.style.width = size + '%';
@@ -18,6 +20,7 @@ function game(sides) {
     }
 }
 
+function etch() {
 const boxes = document.querySelectorAll('.box');
 boxes.forEach((box) => {
     box.addEventListener('mouseover', () => {
@@ -27,25 +30,39 @@ boxes.forEach((box) => {
         //setTimeout(function () { box.style.background = "lightgrey" }, 500);
     })
 })
-
-
+}
 
 document.getElementById("clear").onclick = function () { clear() };
 
 function clear() {
+    const boxes = document.querySelectorAll('.box');
     boxes.forEach((box) => {
         box.style.background = "lightgrey";
     })
 }
 
 
-document.getElementById("changebg").onclick = function () { changebg() };
+document.getElementById("changebg").onclick = function () { changeBg() };
 
-function changebg() {
+function changeBg() {
+    const boxes = document.querySelectorAll('.box');
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     boxes.forEach((box) => {
     box.style.backgroundColor = '#' + randomColor;
     }
     )
 }
+
+document.getElementById("newgrid").onclick = function () { newGrid() };
+
+function newGrid() {
+    let allBoxes = container.getElementsByClassName('box');
+    [].forEach.call(document.querySelectorAll('.box'),function(e){
+        e.parentNode.removeChild(e);
+      });
+}
+
+document.getElementById("test").onclick = function () { game(16); 
+   etch()};
+
 
